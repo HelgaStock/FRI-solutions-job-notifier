591a0a2f7523bb7073433f78aa9c5d6e6cc5b09c
Helga Stock <helga.stock@me.com>
2022-10-31 16:00:20 +0100

    job list

--- api-server/src/main.py
+++ api-server/src/main.py
@@ -5,3 +5,21 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"health": "OK"}
+
+@app.get("/job")
+def getJobs():
+    return [{
+        "id": "uid1",
+        "title": "cinematographer",
+        "description": "head of imagery",
+        },{
+        "id": "uid2",
+        "title": "camera operator",
+        "description": "operates camera",
+        },{
+        "id": "uid3",
+        "title": "steadicam operator",
+        "description": "smooth camera movement",
+        }]
+        
+
